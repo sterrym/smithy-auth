ea53f917b67b7f0bd8d732175b97cece68cae226
Tim Glen <tim@tag.ca>
2013-04-04 12:38:58 -0400

    User model with validations

--- app/models/smithy/user.rb
+++ app/models/smithy/user.rb
@@ -1,5 +1,10 @@
 module Smithy
-  class User
+  class User < ActiveRecord::Base
+    attr_accessible :email, :password, :password_confirmation
+
+    validates_presence_of :email
+    validates_presence_of :password, :on => :create
+
     has_secure_password
 
     def smithy_admin?
