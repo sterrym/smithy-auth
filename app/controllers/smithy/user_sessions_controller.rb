f790c1bd5c4bc2b1ced0ace3a0ef2b215e2a6865
Tim Glen <tim@tag.ca>
2013-04-09 12:44:34 -0400

    login functioning

--- app/controllers/smithy/user_sessions_controller.rb
+++ app/controllers/smithy/user_sessions_controller.rb
@@ -8,10 +8,10 @@ module Smithy
     end
 
     def create
-      user = User.find_by_email(params[:email])
-      if user && user.authenticate(params[:password])
+      user = User.find_by_email(params[:login][:email])
+      if user && user.authenticate(params[:login][:password])
         session[:user_id] = user.id
-        redirect_to smithy_pages_url, :notice => "Logged in!"
+        redirect_to pages_path, :notice => "Logged in!"
       else
         flash.now.alert = "Invalid email or password"
         render "new"
