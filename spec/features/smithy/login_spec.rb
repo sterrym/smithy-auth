8d4308101386b599dc23f94c27bec4f32e45290b
Tim Glen <tim@tag.ca>
2013-10-22 17:19:56 -0400

    fixed the login_spec

--- spec/features/smithy/login_spec.rb
+++ spec/features/smithy/login_spec.rb
@@ -7,12 +7,12 @@ feature "Login" do
 
   scenario "Logging in with correct credentials" do
     visit smithy.login_path
-    within("#session") do
-      fill_in 'Login', :with => 'user@example.com'
+    within("form.login") do
+      fill_in 'Email', :with => 'user@example.com'
       fill_in 'Password', :with => 'secret123'
     end
-    press_button 'Login'
-    page.should have_content 'Success'
+    click_button 'Login'
+    page.should have_content 'Logged in'
   end
 
 end
