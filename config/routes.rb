8c2f0a8f157a234274ad926d591dd87d676457e5
Tim Glen <tim@tag.ca>
2014-03-18 14:40:06 -0400

    Only write the routes if they do not exist

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Smithy::Engine.routes.prepend do
   scope '/smithy' do
-    get '/login' => 'user_sessions#new', :as => :login
-    delete '/logout' => 'user_sessions#destroy', :as => :logout
+    get '/login' => 'user_sessions#new', :as => :login unless has_named_route?(:login)
+    delete '/logout' => 'user_sessions#destroy', :as => :logout unless has_named_route?(:logout)
     resource :user_session, :only => [:new, :create, :destroy]
     resources :users
   end
